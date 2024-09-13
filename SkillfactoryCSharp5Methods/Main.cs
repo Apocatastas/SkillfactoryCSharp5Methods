9f95500a4f4118f29b8eb8c0b760ea396ef876c3
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-13 21:56:20 +0300

    task 5.1.5

--- SkillfactoryCSharp5Methods/Main.cs
+++ SkillfactoryCSharp5Methods/Main.cs
@@ -1,27 +1,74 @@
 ﻿using System;
 
-
-
 class MainClass
 {
+
+    static string ShowColor()
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        var color = Console.ReadLine();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+        return color;
+    }
+
     public static void Main(string[] args)
     {
 
-        (string Name, string[] Dishes) User;
-
-        User.Dishes = new string[5];
+        var (name, age) = ("Евгения", 27);
 
         Console.Write("Введите имя: ");
-        User.Name = Console.ReadLine();
+        name = Console.ReadLine();
+        Console.Write("Введите возрас с цифрами:");
+        age = Convert.ToInt32(Console.ReadLine());
 
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
 
-        for (int i = 0; i<User.Dishes.Length; i++ )
+        string[] favcolors = new string[3];
+
+        for (int i = 0; i < favcolors.Length; i++)
         {
-            Console.Write("\nВведите ваше любимое блюдо {0}: ", i+1);
-            User.Dishes[i] = Console.ReadLine();
+            favcolors[i] = ShowColor();
         }
 
-        Console.ReadLine();
+        Console.WriteLine("\nВаши любимые цвета: ");
+            
+        foreach (string item in favcolors)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
+        
+
+
     }
 }
-
