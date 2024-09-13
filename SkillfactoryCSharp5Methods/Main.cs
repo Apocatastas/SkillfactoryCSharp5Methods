200992bb461a0437187b3110d3dcf2d6f8417b55
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-13 21:33:40 +0300

    task 5.1.1

--- SkillfactoryCSharp5Methods/Main.cs
+++ SkillfactoryCSharp5Methods/Main.cs
@@ -7,6 +7,20 @@ class MainClass
     public static void Main(string[] args)
     {
 
+        (string Name, string[] Dishes) User;
+
+        User.Dishes = new string[5];
+
+        Console.Write("Введите имя: ");
+        User.Name = Console.ReadLine();
+
+
+        for (int i = 0; i<5; i++ )
+        {
+            Console.Write("\nВведите ваше любимое блюдо {0}: ", i+1);
+            User.Dishes[i] = Console.ReadLine();
+        }
+
         Console.ReadLine();
     }
 }
