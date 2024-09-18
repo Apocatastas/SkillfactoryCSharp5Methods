8b77ddf7c94913ac90c0d2d20bd54020fa8afe5e
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-18 23:30:21 +0300

    task 5.3.13

--- SkillfactoryCSharp5Methods/Main.cs
+++ SkillfactoryCSharp5Methods/Main.cs
@@ -2,34 +2,94 @@
 
 class MainClass
 {
-    static void GetName(string name)
+    const int arrLen = 3;
+
+
+    static void SortArray(in int[] arr, out int[] sortedAsc, out int[] sortedDesc)
     {
-        Console.WriteLine("Введите имя");
-        name = Console.ReadLine();
+        sortedAsc = SortArrayAsc(arr);
+        sortedDesc = SortArrayDesc(arr);
 
     }
 
-    static void ChangeAge(ref int age)
-    {
-        age = 15;
 
+    static int[] SortArrayDesc(int[] arr)
+    {
+        int buf = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] < arr[j])
+                {
+                    buf = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = buf;
+                }
+            }
+        }
+
+        return arr;
     }
 
+    static int[] SortArrayAsc(int[] arr)
+    {
+        int buf = 0;
+       for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    buf = arr[i];
+                    arr[i] = arr[j];
+                   arr[j] = buf;
+                }
+            }
+       }
+
+        return arr;
+    }
+    
+
+    static int[] GetArrayFromConsole(int len = arrLen)
+    {
+        var result = new int[len];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+
+        return result;
+    }
+
+    public static void ShowArray(in int[] arr, out int[] sortedAsc, out int[] sortedDesc)
+    {
+        SortArray(in arr, out sortedAsc, out sortedDesc);
+
+
+            Console.WriteLine("Ascending: ");
+            foreach (var item in sortedAsc)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nDescending: ");
+            foreach (var item in sortedDesc)
+            {
+                Console.Write(item + " ");
+            }
+
+    }
 
     public static void Main(string[] args)
     {
-        string name = "Алла";
-        int age = 0;
-        /* 
-         Console.WriteLine(name);
-         GetName(name);
-         Console.WriteLine(name);
-        */
-
-        Console.WriteLine(age);
-        ChangeAge(ref age);
-        Console.WriteLine(age);
-
+        var sortedAsc = new int[arrLen];
+        var sortedDesc = new int[arrLen];
+        int[] arrForSort = new int[arrLen];
+        arrForSort = GetArrayFromConsole();
+        ShowArray(in arrForSort, out sortedAsc, out sortedDesc);
         Console.ReadKey();
 
     }
