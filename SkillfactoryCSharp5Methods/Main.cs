9875c10ee8da8c2ba49be8511d951c44f67a7b75
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-19 17:04:16 +0300

    task 5.5.8

--- SkillfactoryCSharp5Methods/Main.cs
+++ SkillfactoryCSharp5Methods/Main.cs
@@ -2,37 +2,42 @@
 
 class MainClass
 {
+    private static int PowerUp(int N, byte pow)
+    {
+        if (pow == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Напишите что-то");
-        var str = Console.ReadLine();
+        Console.WriteLine("Основание: ");
+        var number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Степень: ");
+        byte pow = Convert.ToByte(Console.ReadLine());
+
+            number = PowerUp(number, pow);
+
+
+
+        Console.WriteLine(number);
         
-
-        Console.WriteLine("Укажите глубину эха");
-        var deep = int.Parse(Console.ReadLine());
-
-        Echo(str, deep);
-
         Console.ReadKey();
     }
 
-    static void Echo(string phrase, int deep)
-    {
-
-        var modif = phrase;
-        Console.BackgroundColor = (ConsoleColor)deep;
-        Console.WriteLine("..." + modif);
-
-
-        if (deep > 1)
-        {
-            if (modif.Length > 2)
-            {
-                modif = modif.Remove(0, 2);
-            }
-            Echo(modif, deep - 1);
-        }
-
+   
 
     }
-}
